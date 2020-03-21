c0289ae4fc1e4ba5e84ae57978a1ecb934cdcb12
Richard Waltman <retiutut@icloud.com>
2020-03-21 18:48:43 -0500

    Return CSV2JSON using promise and await in test

--- test/index.js
+++ test/index.js
@@ -1,24 +1,27 @@
 import { assert } from 'chai';
 import isAO from './helpers';
-import defaultAwesomeFunction, { awesomeFunction, csvToJson, directoryToJson } from '../src';
+import { csvToJson, directoryToJson } from '../src/index';
+import { runNN } from '../src/neuralNetTest';
 
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh';
-    assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
-  });
 
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function';
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
+describe('Neural Net Test', () => {
+  it('Should perform a basic test on the neural net', () => {
+    const result = runNN(1, 0.2, 0);
+    // eslint-disable-next-line no-console
+    console.log(result);
+    assert.isTrue(result.black <= 0.04 && result.white >= 0.96, "Something went wrong with the neural network :'(");
   });
 });
 
 describe('Convert Directory and CSV to JSON for processing.', () => {
-  it('Should output two JSONs', () => {
-    const check = isAO(csvToJson());
-    const check2 = isAO(directoryToJson());
-    assert.isTrue(check, 'CSV is valid');
-    assert.isTrue(check2, 'Directory is valid');
+  it('Should output two JSONs', async () => {
+    const database = await csvToJson();
+    const directory = directoryToJson();
+    // eslint-disable-next-line no-console
+    console.log(database);
+    // eslint-disable-next-line no-console
+    console.log(directory);
+    assert.isTrue(isAO(database), 'CSV is not valid JSON');
+    assert.isTrue(isAO(directory), 'Directory is not valid JSON');
   });
 });
